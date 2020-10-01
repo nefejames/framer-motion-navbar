5d88c8c5f8de9a7523e77becd6dc4b20d36d746d
nefejames <nefejames@yahoo.com>
2020-10-01 00:08:27 -0700

    framer motion navbar

---
+++ src/Styles.js
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+export const Header = styled.header`
+  background: green;
+  position: relative;
+  display: flex;
+  padding: 1rem;
+  justify-content: flex-end;
+  z-index: 2;
+`;
+
+export const Nav = styled(motion.nav)`
+  background-color: red;
+  height: 90vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Link = styled(motion.li)`
+  color: white;
+  margin-bottom: 1.6rem;
+  font-size: 1.4rem;
+`;
+
+export const SvgBox = styled(motion.div)``;
